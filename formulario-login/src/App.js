2d18a42d4432ea0ea4b923abb196ef429b197d79
Thaylan Neto <thaaylann20v@gmail.com>
2025-02-21 12:54:14 -0300

    Mudança de título

--- formulario-login/src/App.js
+++ formulario-login/src/App.js
@@ -19,7 +19,7 @@ function App() {
 
   return (
     <div className="App">
-      <h2>Formulário de Login</h2>
+      <h2>BEM VINDO</h2>
       <form onSubmit={handleLogin}>
         <div>
           <label>Login:</label>
